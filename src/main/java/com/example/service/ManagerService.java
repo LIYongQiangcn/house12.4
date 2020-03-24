1016576b2c18f3526ac6b4d26e58907e5f145649
lyq <987274643@qq.com>
2020-03-24 10:31:14 +0800

    2020.3.24添加

---
+++ src/main/java/com/example/service/ManagerService.java
@@ -0,0 +1,36 @@
+package com.example.service;
+
+import com.example.entity.Manager;
+import com.example.mapper.ManagerMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author {李永强}
+ * @Title ManagerMapper
+ * @Description
+ * @date 2020/3/20
+ */
+@Service
+public class ManagerService {
+
+    @Resource
+    ManagerMapper managerMapper;
+
+    /**
+     * 管理员登录
+     */
+    public Manager login(String munber , String mpassword ){
+        Manager manager = managerMapper.managerlogin(munber,mpassword);
+        return manager;
+    }
+
+    /**
+     * 会员密码修改
+     */
+    public int  update(Manager manager){
+        return managerMapper.updatePassword(manager);
+    }
+
+}
